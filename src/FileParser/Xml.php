8a0e3c66602aaee7857b74c93bcda0e1700cbf8d
Vincent Gabriel <vadimg88@gmail.com>
2015-10-15 09:31:04 -0700

    added ability to pass in a string to the XML parser

--- src/FileParser/Xml.php
+++ src/FileParser/Xml.php
@@ -17,15 +17,19 @@ class Xml implements FileParserInterface
 {
     /**
      * {@inheritDoc}
-     * Parses an XML file as an array
+     * Parses an XML file or string as an array
      *
      * @throws ParseException If there is an error parsing the XML file
      */
-    public function parse($path)
+    public function parse($pathOrString)
     {
         libxml_use_internal_errors(true);
-
-        $data = simplexml_load_file($path, null, LIBXML_NOERROR);
+    
+        if(is_file($pathOrString)) {
+            $data = simplexml_load_file($pathOrString, null, LIBXML_NOERROR);
+        } else {
+            $data = simplexml_load_string($pathOrString, 'SimpleXMLElement', LIBXML_NOERROR | LIBXML_PARSEHUGE);
+        }
 
         if ($data === false) {
             $errors      = libxml_get_errors();
